8ad54b2952414bda7145cce7acd2cce5e3ac0c45
Harper Bellows <harper.bellows@gmail.com>
2018-05-19 13:06:34 -0600

    add board coordinates

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,14 +2,15 @@ class Computer
   attr_reader :coords
   def initialize
     @coords = ['A1', 'A2', 'A3', 'A4',
-              'B1', 'B2', 'B3', 'B4',
-              'C1', 'C2', 'C3', 'C4',
-              'D1', 'D2', 'D3', 'D4']
-
+               'B1', 'B2', 'B3', 'B4',
+               'C1', 'C2', 'C3', 'C4',
+               'D1', 'D2', 'D3', 'D4']
   end
 
-  # def coordinates
-  #
-  #
-  # end
+  def ship_placement
+    ship_coords = []
+    @coords.each do |index|
+      ship_coord << index.sample
+    end
+  end
 end
