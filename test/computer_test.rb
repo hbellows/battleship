6e6ee086a780a86fe49c697a63a2f9b8b3ed5afe
Harper Bellows <harper.bellows@gmail.com>
2018-05-19 13:06:57 -0600

    add test for board coordinates

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -7,6 +7,15 @@ class ComputerTest < Minitest::Test
     computer = Computer.new
 
     assert_equal 'A1', computer.coords[0]
+    assert_equal 'B2', computer.coords[5]
+    assert_equal 'C3', computer.coords[10]
+    assert_equal 'D4', computer.coords[15]
+  end
 
+  def test_it_can_choose_a_random_coordinate
+    computer = Computer.new
+    ship_coords = computer.ship_placement
+
+    assert_includes ship_coords, computer.coords
   end
 end
