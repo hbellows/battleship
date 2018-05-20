90786f13785a0182ccddca8422a9655ca0173ea2
Harper Bellows <harper.bellows@gmail.com>
2018-05-20 11:48:33 -0600

    add tests for ship building validation

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,50 +4,50 @@ require '.lib/player'
 class PlayerTest < Minitest::Test
 
   def test_it_can_find_coordinates
-    computer = Player.new
+    player = Player.new
 
-    assert_equal 'A1', computer.board[0]
-    assert_equal 'B2', computer.board[5]
-    assert_equal 'C3', computer.board[10]
-    assert_equal 'D4', computer.board[15]
+    assert_equal 'A1', player.board[0]
+    assert_equal 'B2', player.board[5]
+    assert_equal 'C3', player.board[10]
+    assert_equal 'D4', player.board[15]
   end
 
   def test_it_can_choose_1st_ship_coord_2_unit_ship
-    comp = Player.new
+    player = Player.new
 
-    comp.first_coord_2_unit
-    assert_includes comp.board, comp.ship_2_unit[0]
+    player.first_coord_2_unit
+    assert_includes player.board, player.ship_2_unit[0]
   end
 
   def test_it_can_choose_2nd_ship_coord_2_unit_ship
-    comp = Player.new
+    player = Player.new
 
-    comp.first_coord_2_unit
-    comp.second_coord_2_unit
-    assert_includes comp.board, comp.ship_2_unit[1]
+    player.first_coord_2_unit
+    player.second_coord_2_unit
+    assert_includes player.board, player.ship_2_unit[1]
   end
 
   def test_it_can_choose_1st_ship_coord_3_unit_ship
-    comp = Player.new
+    player = Player.new
 
-    comp.first_coord_3_unit
-    assert_includes comp.board, comp.ship_3_unit[0]
+    player.first_coord_3_unit
+    assert_includes player.board, player.ship_3_unit[0]
   end
 
   def test_it_can_choose_2nd_ship_coord_3_unit_ship
-    comp = Player.new
+    player = Player.new
 
-    comp.first_coord_3_unit
-    comp.second_coord_3_unit
-    assert_includes comp.board, comp.ship_3_unit[1]
+    player.first_coord_3_unit
+    player.second_coord_3_unit
+    assert_includes player.board, player.ship_3_unit[1]
   end
 
   def test_it_can_choose_3rd_ship_coord_3_unit_ship
-    comp = Player.new
+    player = Player.new
 
-    comp.first_coord_3_unit
-    comp.second_coord_3_unit
-    comp.third_coord_3_unit
-    assert_includes comp.board, comp.ship_3_unit[2]
+    player.first_coord_3_unit
+    player.second_coord_3_unit
+    player.third_coord_3_unit
+    assert_includes player.board, player.ship_3_unit[2]
   end
 end
