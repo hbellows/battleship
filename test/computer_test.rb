956fb04b05ec1314521cf76ba1b002bba71d83b2
Harper Bellows <harper.bellows@gmail.com>
2018-05-20 11:45:47 -0600

    standardize test language

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -4,12 +4,12 @@ require './lib/computer'
 class ComputerTest < Minitest::Test
 
   def test_it_can_find_coordinates
-    computer = Computer.new
+    comp = Computer.new
 
-    assert_equal 'A1', computer.board[0]
-    assert_equal 'B2', computer.board[5]
-    assert_equal 'C3', computer.board[10]
-    assert_equal 'D4', computer.board[15]
+    assert_equal 'A1', comp.board[0]
+    assert_equal 'B2', comp.board[5]
+    assert_equal 'C3', comp.board[10]
+    assert_equal 'D4', comp.board[15]
   end
 
   def test_it_can_choose_1st_ship_coord_2_unit_ship
