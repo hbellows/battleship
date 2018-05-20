79a10b655adb63a8825eedd68b4dc40ac4480fd5
Harper Bellows <harper.bellows@gmail.com>
2018-05-20 11:44:56 -0600

    WIP

--- test/player_test.rb
+++ test/player_test.rb
@@ -3,7 +3,51 @@ require '.lib/player'
 
 class PlayerTest < Minitest::Test
 
-  def test_case_name
+  def test_it_can_find_coordinates
+    computer = Player.new
 
+    assert_equal 'A1', computer.board[0]
+    assert_equal 'B2', computer.board[5]
+    assert_equal 'C3', computer.board[10]
+    assert_equal 'D4', computer.board[15]
+  end
+
+  def test_it_can_choose_1st_ship_coord_2_unit_ship
+    comp = Player.new
+
+    comp.first_coord_2_unit
+    assert_includes comp.board, comp.ship_2_unit[0]
+  end
+
+  def test_it_can_choose_2nd_ship_coord_2_unit_ship
+    comp = Player.new
+
+    comp.first_coord_2_unit
+    comp.second_coord_2_unit
+    assert_includes comp.board, comp.ship_2_unit[1]
+  end
+
+  def test_it_can_choose_1st_ship_coord_3_unit_ship
+    comp = Player.new
+
+    comp.first_coord_3_unit
+    assert_includes comp.board, comp.ship_3_unit[0]
+  end
+
+  def test_it_can_choose_2nd_ship_coord_3_unit_ship
+    comp = Player.new
+
+    comp.first_coord_3_unit
+    comp.second_coord_3_unit
+    assert_includes comp.board, comp.ship_3_unit[1]
+  end
+
+  def test_it_can_choose_3rd_ship_coord_3_unit_ship
+    comp = Player.new
+
+    comp.first_coord_3_unit
+    comp.second_coord_3_unit
+    comp.third_coord_3_unit
+    assert_includes comp.board, comp.ship_3_unit[2]
   end
 end
