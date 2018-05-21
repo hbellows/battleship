bed48306c38d6e262d229999669573dc8c9871f5
Harper Bellows <harper.bellows@gmail.com>
2018-05-21 07:05:12 -0600

    WIP

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,7 +4,7 @@ require './lib/player'
 class PlayerTest < Minitest::Test
 
   def test_it_can_find_coordinates
-    skip
+    # skip
     player = Player.new
 
     assert_equal 'A1', player.board[0]
@@ -14,7 +14,7 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_can_validate_user_1st_ship_input
-    skip
+    # skip
     player = Player.new
 
     assert player.coord_1_valid?('A3')
@@ -22,25 +22,28 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_can_add_1st_ship_coord_2_unit_ship
-    skip
+    # skip
     player = Player.new
 
     player.first_coord_2_unit('A1')
     assert_equal 'A1', player.ship_2_unit[0]
+
+    player.first_coord_2_unit('f3')
+    refute_equal 'F3', player.ship_2_unit[0]
   end
 
   def test_it_can_validate_user_2nd_ship_input
-    skip
+    # skip
     player = Player.new
 
     player.first_coord_2_unit('a1')
-    player.first_coord_2_unit('a2')
+    # player.second_coord_2_unit('a2')
     assert player.coord_2_valid?('a2')
     refute player.coord_2_valid?('A4')
   end
 
   def test_it_can_add_2nd_ship_coord_2_unit_ship
-    skip
+    # skip
     player = Player.new
 
     player.first_coord_2_unit('B2')
@@ -49,17 +52,29 @@ class PlayerTest < Minitest::Test
     assert_equal 'C2', player.ship_2_unit[1]
   end
 
-  def test_it_can_validate_user_3rd_ship_input
-    skip
+  def test_it_can_detect_invalid_user_input
+    # skip
     player = Player.new
 
-    player.first_coord_3_unit('B1')
-    assert player.coord_3_valid?
+    player.first_coord_2_unit('B2')
+    player.second_coord_2_unit('F2')
+    assert_equal 'B2', player.ship_2_unit[0]
+    refute_equal 'F2', player.ship_2_unit[1]
+  end
 
+  def test_it_can_validate_user_3rd_ship_input
+    # skip
+    player = Player.new
+
+    player.first_coord_2_unit('A4')
+    player.second_coord_2_unit('B4')
+
+    assert player.coord_3_valid?('B1')
+    refute player.coord_3_valid?('B4')
   end
 
   def test_it_can_add_1st_ship_coord_3_unit_ship
-    skip
+    # skip
     player = Player.new
 
     player.first_coord_3_unit('D4')
@@ -67,11 +82,14 @@ class PlayerTest < Minitest::Test
   end
 
   def test_it_can_validate_user_4th_ship_input
-    skip
+    # skip
     player = Player.new
 
-    player.second_coord_3_unit('c2')
-    assert player.coord_4_valid?
+    player.first_coord_2_unit('A4')
+    player.second_coord_2_unit('B4')
+
+    assert player.coord_3_valid?('B1')
+    refute player.coord_3_valid?('B4')
   end
 
   def test_it_can_add_2nd_ship_coord_3_unit_ship
