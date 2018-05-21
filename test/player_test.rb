2eef319ba4f030a5ff546a8fd8fd928292b5d357
Harper Bellows <harper.bellows@gmail.com>
2018-05-21 07:58:17 -0600

    add final validation tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -96,24 +96,33 @@ class PlayerTest < Minitest::Test
     # skip
     player = Player.new
 
+    player.first_coord_2_unit('A4')
+    player.second_coord_2_unit('B4')
     player.first_coord_3_unit('A2')
     player.second_coord_3_unit('A1')
-    assert_equal 'A4', player.ship_3_unit[0]
+    assert_equal 'A2', player.ship_3_unit[0]
     assert_equal 'A1', player.ship_3_unit[1]
   end
 
   def test_it_can_validate_user_5th_ship_input
-    skip
+    # skip
     player = Player.new
 
-    player.third_coord_3_unit('d4')
-    assert player.coord_5_valid?
+    player.first_coord_2_unit('A4')
+    player.second_coord_2_unit('B4')
+    player.first_coord_3_unit('D2')
+    player.second_coord_3_unit('D3')
+
+    assert player.coord_5_valid?('D4')
+    refute player.coord_5_valid?('D1')
   end
 
   def test_it_can_add_3rd_ship_coord_3_unit_ship
-    skip
+    # skip
     player = Player.new
 
+    player.first_coord_2_unit('A4')
+    player.second_coord_2_unit('B4')
     player.first_coord_3_unit('A1')
     player.second_coord_3_unit('B1')
     player.third_coord_3_unit('C1')
